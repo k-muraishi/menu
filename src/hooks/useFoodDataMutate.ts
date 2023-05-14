a37f8e8205e772b9f6f67733e3602acc2c2497f8
“Kohei <muraishi634@gmail.com>
2023-05-14 17:21:32 +0900

    url修正

--- src/hooks/useFoodDataMutate.ts
+++ src/hooks/useFoodDataMutate.ts
@@ -2,8 +2,7 @@ import axios, { AxiosPromise } from "axios";
 import { FoodData } from "../interface/FoodData";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const API_URL = "https://c4bc-60-88-229-123.ngrok-free.app";
-
+const API_URL = "https://5b38-60-88-229-123.ngrok-free.app";
 const postData = async (data: FoodData): AxiosPromise<any> => {
   const response = axios.post(API_URL + "/food", data);
   return response;
